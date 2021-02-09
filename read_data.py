9a7c59993575d47c865edf8fdcd0f5108b1a5087
agoryelov <andrey.goryelov@gmail.com>
2021-02-08 21:10:45 -0800

    first commit

---
+++ read_data.py
@@ -0,0 +1,10 @@
+import h5py
+
+# filename = "/fast_scratch/WatChMaL/data/IWCD_mPMT_Short_emg_E0to1000MeV_digihits.h5"
+filename = "/home/esuom/Downloads/IWCDmPMT_4pi_fulltank_test_graphnet.h5"
+
+h5_file = h5py.File(filename,'r')
+
+print(list(h5_file.keys()))
+
+h5_file.close()
